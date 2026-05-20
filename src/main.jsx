5b82e3e8f28ff5acf206bdee0469da7ad8f1c95f
Oguz Kutlu <oguzkkutlu@gmail.com>
2026-05-20 17:37:14 +0300

    analytics final fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
+
 import { Analytics } from '@vercel/analytics/react'
 
-createRoot(document.getElementById('root')).render(
-<>
-  <App />
-  <Analytics />
-</>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>,
 )
